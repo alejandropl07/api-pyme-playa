54b0b8faf269b126887fe5b9064fe04a480233f0
rdiazca <rdiazca96@gmail.com>
2022-04-20 15:03:33 -0400

    Actualizada contraseña de conexión a mysql

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize ('unevol_db', 'root', 'rndc960604',{
+const db = new Sequelize ('unevol_db', 'root', '',{
     host: 'localhost',
     dialect: 'mysql',
     define: {
