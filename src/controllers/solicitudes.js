eac64883ea19fa3fcd5237b435739975a5f669aa
rdiazca <rdiazca96@gmail.com>
2022-04-24 02:04:14 -0400

    Agregado método para obtener una solicitud dado su id (getSolicitud)

--- src/controllers/solicitudes.js
+++ src/controllers/solicitudes.js
@@ -1,8 +1,29 @@
 import Solicitud from "../models/solicitud.js"
 
+export const getSolicitudes = async (req, res) => {
+    const solicitudes = await Solicitud.findAll({
+        attributes: ['id_solicitud', 'descrip_solicitud']
+    });
+
+    res.json({solicitudes});
+}
+
+export const getSolicitud = async (req, res) => {
+    const {id} = req.params;
+    const solicitud = await Solicitud.findByPk(id);
+
+    if( solicitud ){
+        res.json(solicitud);
+    } else{
+        res.status(404).json({
+            msg: `No existe una solicitud con el id ${id}`
+        });
+    }
+
+}
+
 export const postSolicitud = async (req, res) => {
     const { body } = req;
-    console.log(body);
 
     try{
         
@@ -16,10 +37,4 @@ export const postSolicitud = async (req, res) => {
         })
     }
 }
-export const getSolicitudes = async (req, res) => {
-    const solicitudes = await Solicitud.findAll({
-        attributes: ['id_solicitud', 'descrip_solicitud']
-    });
 
-    res.json({solicitudes});
-}
--- src/routes/solicitud.js
+++ src/routes/solicitud.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { getSolicitudes, 
+         getSolicitud,
          postSolicitud 
     } from "../controllers/solicitudes.js";
 
 const router = Router();
 
 router.get('/', getSolicitudes);
+router.get('/:id', getSolicitud);
 router.post('/', postSolicitud);
 
 export default router;
